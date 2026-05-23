5a3949bb6572b78da5267478cf1348d8fbe2dbfa
TiewKH <tiewkeehui95@hotmail.com>
2026-05-23 12:52:43 +0800

    feat: add sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 import { defineConfig } from "astro/config";
 
+import sitemap from "@astrojs/sitemap";
+
 export default defineConfig({
   site: "https://tiewkeehui.cc",
+
   markdown: {
     shikiConfig: {
       theme: "github-dark",
       wrap: true
     }
-  }
+  },
+
+  integrations: [sitemap()]
 });
